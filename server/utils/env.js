be578a8917f7bbf46372aac47274afaf803558d8
Anneraymond08 <90001135+Anneraymond08@users.noreply.github.com>
2022-11-21 09:08:08 -0500

    AR: connection a la mongoDB

--- server/utils/env.js
+++ server/utils/env.js
@@ -3,7 +3,8 @@ const DB_COLLECTION_SONGS = "songs";
 const DB_COLLECTION_PLAYLISTS = "playlists";
 
 // TODO : fournir les bonnes informations de connexion (les <> doivent être enlevés)
-const DB_URL = "mongodb+srv://<user>:<password>@cluster0.ldkwk.mongodb.net/?retryWrites=true&w=majority";
+//const DB_URL = "mongodb+srv://<user>:<password>@cluster0.ldkwk.mongodb.net/?retryWrites=true&w=majority";
+const DB_URL = "mongodb+srv://Admin:creator22tp5@cluster0.t6bcpqe.mongodb.net/?retryWrites=true&w=majority";
 
 exports.DB_DB = DB_DB;
 exports.DB_COLLECTION_SONGS = DB_COLLECTION_SONGS;
